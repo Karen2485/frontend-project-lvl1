08242ed775ac6c697e99c1513b41b207d6c9887b
Karen <asatryan_karen@mail.ru>
2022-06-14 23:29:11 +0300

    step 8 of project

--- games/gcd.js
+++ games/gcd.js
@@ -1,12 +1,12 @@
-import readLineSync from 'readline-sync';
 import {
-  congratulation, correctAnswer, randomNumber, welcome, wrongAnswer,
+  congratulation, correctAnswer, randomNumber, welcome, wrongAnswer, answer, haveName,
+  greeting,
 } from '../src/index.js';
 
 const greatestCommonDivisor = () => {
   welcome();
-  const name = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const name = haveName();
+  greeting(name);
   console.log('Find the greatest common divisor of given numbers.');
   for (let i = 1; i <= 3; i += 1) {
     let number1 = (randomNumber(150) + 1);
@@ -21,7 +21,7 @@ const greatestCommonDivisor = () => {
       }
       divider = number1;
     }
-    const userAnswer = Number(readLineSync.question('Your answer: '));
+    const userAnswer = Number(answer());
     if (userAnswer !== divider) {
       wrongAnswer(userAnswer, divider, name);
       return;
