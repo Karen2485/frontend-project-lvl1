cb128c4e54be44a60a945c58f64c17ba201d89de
Karen <asatryan_karen@mail.ru>
2022-06-17 00:03:48 +0300

    step 9 of project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import game from '../games/even.js';
 
-console.log('Welcome to the Brain Games!');
 game();
--- games/even.js
+++ games/even.js
@@ -1,8 +1,13 @@
 import readLineSync from 'readline-sync';
+import {
+  congratulation, correctAnswer, randomNumber, welcome, wrongAnswer, answer, haveName,
+  greeting,
+} from '../src/index.js';
 
 const game = () => {
-  const name = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  welcome();
+  const name = haveName();
+  greeting(name);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
     const isEven = Math.round(Math.random() * 100);
@@ -12,16 +17,16 @@ const game = () => {
     if (isEven % 2 !== 0 && userAnswer === 'no') {
       console.log('Correct!');
     } else if (isEven % 2 !== 0 && userAnswer !== 'no') {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${name}!`);
       return;
     }
     if (isEven % 2 === 0 && userAnswer === 'yes') {
       console.log('Correct!');
     } else if (isEven % 2 === 0 && userAnswer !== 'yes') {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${name}!`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
       return;
     }
   }
-  console.log(`Congratulations, ${name}!`);
+  congratulation(name);
 };
 export default game;
