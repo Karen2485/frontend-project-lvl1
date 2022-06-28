fbd274903aeb7024cec0d21c218e83ed597581f5
Karen <asatryan_karen@mail.ru>
2022-06-28 09:24:19 +0300

    fix project

--- src/games/even.js
+++ src/games/even.js
@@ -1,12 +1,7 @@
 import randomNumber from '../helpers.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-};
+const isEven = (number) => number % 2 === 0;
 const getGameData = () => {
   const givenNumber = randomNumber(0, 100);
   const gameTask = `${givenNumber}`;
